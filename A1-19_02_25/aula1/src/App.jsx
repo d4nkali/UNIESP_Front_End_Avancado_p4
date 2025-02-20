bdc97fa384d76e015ebf9b40edbd09e63788aac6
d4nkali <133923419+d4nkali@users.noreply.github.com>
2025-02-20 10:57:01 -0300

    Criando componetes adicao e subtracao e exebindo no componente pai app

--- A1-19_02_25/aula1/src/App.jsx
+++ A1-19_02_25/aula1/src/App.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import Adicao from "./components/Adicao";
+import Subtracao from "./components/Subitracao";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+    return (
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
 
-export default App
+        <Adicao num1={4} num2={6} />
+        <Subtracao num1={8} num2={2} />
+
+    </div>)
+};
+
+export default App;
