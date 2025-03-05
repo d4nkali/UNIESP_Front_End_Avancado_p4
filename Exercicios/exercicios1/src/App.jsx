24e3c2cd496019f4dd3ca84d777b235f273419b4
d4nkali <133923419+d4nkali@users.noreply.github.com>
2025-03-05 10:22:03 -0300

    Adicionando componente filho PrecisoEstudar

--- Exercicios/exercicios1/src/App.jsx
+++ Exercicios/exercicios1/src/App.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+
 import Adicao from "./components/Adicao";
 import Subtracao from "./components/Subtracao";
 import Multiplicacao from "./components/Multiplicacao";
 import Divisao from "./components/Divisao";
+import PrecisoEstudar from "./components/PrecisoEstudar";
+
 
 function App() {
 
@@ -13,7 +16,7 @@ function App() {
             <h1>Exercícios Aula 1 React</h1>
             <hr />
 
-            <h3>Exercícios 1</h3>
+            <h3>Exercício 1</h3>
 
             <Adicao num1={2} num2={3}></Adicao>
             <br />
@@ -27,9 +30,17 @@ function App() {
             <Divisao num1={90} num2={3}></Divisao>
             <br />
 
+            <br />
+            <hr />
+            <h3>Exercício 2</h3>
+
+            <PrecisoEstudar nomeDaTecnologia={"TypeScript"}></PrecisoEstudar>
+            <br />
+
         </>
 
     );
+
 }
 
 export default App;
