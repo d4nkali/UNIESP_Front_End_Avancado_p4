16a3674c18e3a0c7f7ba034d73343d815f620e90
d4nkali <133923419+d4nkali@users.noreply.github.com>
2025-03-12 20:34:44 -0300

    Adicionando exemplo 2

--- A2-12_03_25/aula2/src/App.jsx
+++ A2-12_03_25/aula2/src/App.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 import RenderizandoComFuncoes from "./components/RenderizandoComFuncoes";
+import RenderizaBackOuFront from "./components/RenderizaBackOuFront";
 
 function App() {
 
     return (
 
-        <RenderizandoComFuncoes />
+        <div>
+
+            <h1>Exercicio 1</h1>
+            <hr />
+            <RenderizandoComFuncoes />
+
+            <br />
+            <hr />
+            <br />
+
+            <h1>Exercicio 2</h1>
+            <hr />
+            <RenderizaBackOuFront />
+
+        </div>
 
     );
 
