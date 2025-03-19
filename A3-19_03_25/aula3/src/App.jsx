86d4be997ea94154668b0792d47d17bd7b8142a3
d4nkali <133923419+d4nkali@users.noreply.github.com>
2025-03-19 16:18:04 -0300

    Adicionando aula 3 exemplo 1

--- A3-19_03_25/aula3/src/App.jsx
+++ A3-19_03_25/aula3/src/App.jsx
@@ -1,24 +1,28 @@
 import React from "react";
 import HookContador from "./components/HookContador";
 import HookMegaSena from "./components/HookMegaSena";
+import EstouConseguindoAprenderReact from "./components/EstouConseguindoAprenderReact";
 
 function App() {
     return (
         <div>
             <h1>Aula 2</h1>
             <hr />
-            <br />
 
             <h2>Exemplo 1</h2>
-            <hr />
             <HookContador />
 
-            <br />
-            <br />
+            <hr />
 
             <h2>Exemplo 2</h2>
-            <hr />
             <HookMegaSena />
+
+            <hr />
+            <h1>Aula 3</h1>
+
+            <h2>Exemplo 1</h2>
+            <EstouConseguindoAprenderReact estouConseguindo={true} />
+
         </div>
     );
 }
