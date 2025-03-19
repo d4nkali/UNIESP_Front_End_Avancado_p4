80aa5161a34fe44e61532b52b5feaef29c51e3a4
d4nkali <133923419+d4nkali@users.noreply.github.com>
2025-03-19 15:45:09 -0300

    Adicionando exemplo 2

--- A3-19_03_25/aula3/src/App.jsx
+++ A3-19_03_25/aula3/src/App.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 import HookContador from "./components/HookContador";
+import HookMegaSena from "./components/HookMegaSena";
 
 function App() {
     return (
         <div>
-            <h1>Exemplo 1</h1>
+            <h1>Aula 2</h1>
+            <hr />
+            <br />
+
+            <h2>Exemplo 1</h2>
             <hr />
             <HookContador />
+
+            <br />
+            <br />
+
+            <h2>Exemplo 2</h2>
+            <hr />
+            <HookMegaSena />
         </div>
     );
-};
+}
 
 export default App;
