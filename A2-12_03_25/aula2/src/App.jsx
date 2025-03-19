4e8c0913412da17a0c7eaa9f1d4dbba16056e975
d4nkali <133923419+d4nkali@users.noreply.github.com>
2025-03-19 20:56:13 -0300

    Movendo local correto

--- A2-12_03_25/aula2/src/App.jsx
+++ A2-12_03_25/aula2/src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import RenderizandoComFuncoes from "./components/RenderizandoComFuncoes";
 import RenderizaBackOuFront from "./components/RenderizaBackOuFront";
 import TrabalhandoComImagens from "./components/TrabalhandoComImagens";
+import HookContador from "./components/HookContador";
+import HookMegaSena from "./components/HookMegaSena";
 
 function App() {
 
@@ -9,25 +11,32 @@ function App() {
 
         <div>
 
-            <h1>Exercicio 1</h1>
-            <hr />
+            <h1>Exercício 1</h1>
             <RenderizandoComFuncoes />
 
             <br />
             <hr />
             <br />
 
-            <h1>Exercicio 2</h1>
-            <hr />
+            <h1>Exercício 2</h1>
             <RenderizaBackOuFront />
 
             <br />
             <hr />
             <br />
 
-            <h1>Exercicio 3</h1>
-            <hr />
+            <h1>Exercício 3</h1>
             <TrabalhandoComImagens />
+
+            <h2>Exercício 4</h2>
+            <HookContador />
+
+            <br />
+            <hr />
+            <br />
+
+            <h2>Exercício 5</h2>
+            <HookMegaSena />
 
         </div>
 
--- A3-19_03_25/aula3/src/App.jsx
+++ A3-19_03_25/aula3/src/App.jsx
@@ -1,21 +1,9 @@
 import React from "react";
-import HookContador from "./components/HookContador";
-import HookMegaSena from "./components/HookMegaSena";
 import EstouConseguindoAprenderReact from "./components/EstouConseguindoAprenderReact";
 
 function App() {
     return (
         <div>
-            <h1>Aula 2</h1>
-            <hr />
-
-            <h2>Exemplo 1</h2>
-            <HookContador />
-
-            <hr />
-
-            <h2>Exemplo 2</h2>
-            <HookMegaSena />
 
             <hr />
             <h1>Aula 3</h1>
