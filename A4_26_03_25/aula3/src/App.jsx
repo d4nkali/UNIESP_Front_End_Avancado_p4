803c0d96f6883b99b86e5985b407a0626fc00af1
d4nkali <133923419+d4nkali@users.noreply.github.com>
2025-03-25 15:01:55 -0300

    Adicionando exemplo 1

---
+++ A4_26_03_25/aula3/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import EstouConseguindoAprenderReact from "./components/EstouConseguindoAprenderReact";
+
+const App = () => {
+    return (
+        <div>
+            <h1>Aula 3</h1>
+            <hr />
+
+            <EstouConseguindoAprenderReact estouConseguindo={true} />
+        </div>
+    );
+};
+
+export default App;
