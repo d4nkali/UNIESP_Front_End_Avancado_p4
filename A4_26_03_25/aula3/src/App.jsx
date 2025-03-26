ebe73ceec95694eaaf7874ef216b593d94c57268
d4nkali <133923419+d4nkali@users.noreply.github.com>
2025-03-26 15:00:53 -0300

    Adicionando Exercicio 3

--- A4_26_03_25/aula3/src/App.jsx
+++ A4_26_03_25/aula3/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import EstouConseguindoAprenderReact from "./components/EstouConseguindoAprenderReact";
 import Aluno from "./components/Aluno";
+import NovoAluno from "./components/NovoAluno";
 
 const App = () => {
 
@@ -12,8 +13,16 @@ const App = () => {
 
     ];
 
+    const novoAlunos = [
+
+        {nome: "João", email: "joao@exemplo.com", curso: "Sistemas para Internet", media: "9.5"},
+        {nome: "Kevin", email: "kevin@exemplo.com", curso: "Sistemas da Informação", media: "6.0"},
+        {nome: "Nicole", email: "nicole@exemplo.com", curso: "Veterinária", media: "8.0"}
+
+    ];
+
     return (
-        <div>
+        <>
             <h1>Aula 3</h1>
             <hr />
 
@@ -29,7 +38,16 @@ const App = () => {
                 <Aluno key={index} {...aluno} />
             ))}
 
-        </div>
+            <br />
+            <hr />
+            <br />
+
+            <h1>Exercício 3</h1>
+            {novoAlunos.map((novoAlunos, index) => (
+                <NovoAluno key={index} {...novoAlunos} />
+            ))}
+
+        </>
     );
 };
 
