67b64f21d326b179e58dd872100a4c999328eb98
d4nkali <133923419+d4nkali@users.noreply.github.com>
2025-04-07 15:45:30 -0300

    Adicionando exercicio 1

--- A5-09_04_25/aula4/src/App.jsx
+++ A5-09_04_25/aula4/src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import EstiloGlobal from "./components/EstiloGlobal";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+    return (
+        <>
+            <h1>Aula 4</h1>
+            <hr />
 
-export default App
+            <h1>Exercício 1</h1>
+            <EstiloGlobal />
+
+            <br />
+            <hr />
+            <br />
+
+        </>
+    );
+};
+
+export default App;
