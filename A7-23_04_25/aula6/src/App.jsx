51bbb121f698f0fca7925c7e64063bdd76150d00
d4nkali <133923419+d4nkali@users.noreply.github.com>
2025-04-23 17:05:01 -0300

    Adicionando exercicio 3

--- A7-23_04_25/aula6/src/App.jsx
+++ A7-23_04_25/aula6/src/App.jsx
@@ -3,6 +3,8 @@ import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Inicial from './components/Inicial';
 import Lgpd from './components/Lgpd';
+import Noticias from './components/Noticias';
+import DetalhesNoticias from './components/DetalhesNoticias';
 
 const App = () => {
 
@@ -13,16 +15,16 @@ const App = () => {
             <h1>Aula 6</h1>
             <hr />
 
-            <h3>Exercício 1 e 2</h3>
+            <h3>Exercício 1, 2 e 3</h3>
             <Navbar />
             <Routes>
 
                 <Route path='/' element={<Inicial />} />
                 <Route path='/lgpd' element={<Lgpd />} />
-                <Route path='/noticias' element={<h1>Noticias</h1>} />
+                <Route path='/noticias' element={<Noticias />} />
+                <Route path='/detalhes-da-noticias/:id' element={<DetalhesNoticias />} />
 
             </Routes>
-
             </BrowserRouter> </>
 
     );
