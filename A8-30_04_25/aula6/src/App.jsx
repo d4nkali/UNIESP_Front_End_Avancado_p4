3d5f92a6827c8df0cd86e8e39027ebd8f5c0b751
d4nkali <133923419+d4nkali@users.noreply.github.com>
2025-04-30 14:50:05 -0300

    Adicionando conteudo passado

--- A8-30_04_25/aula6/src/App.jsx
+++ A8-30_04_25/aula6/src/App.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Inicial from './pages/Inicial';
+import Lgpd from './pages/Lgpd';
+import Noticias from './pages/Noticias';
+import DetalhesNoticias from './components/DetalhesNoticias';
 
 const App = () => {
 
     return (
-    
-        <>
 
-            <h1>Aula 8</h1>
+        <> <BrowserRouter>
+
+            <h1>Continuação da Aula 6</h1>
             <hr />
 
-            
+            <h3>Exercício 1, 2 e 3</h3>
+            <Navbar />
+            <Routes>
 
-        </>
+                <Route path='/' element={<Inicial />} />
+                <Route path='/lgpd' element={<Lgpd />} />
+                <Route path='/noticias' element={<Noticias />} />
+                <Route path='/detalhes-da-noticias/:id' element={<DetalhesNoticias />} />
+
+            </Routes>
+            </BrowserRouter> </>
 
     );
 
