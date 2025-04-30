88a3c47f6416cc0f368fde239a8bb80c551c1be9
d4nkali <133923419+d4nkali@users.noreply.github.com>
2025-04-30 14:40:27 -0300

    Criando projet

---
+++ A8-30_04_25/aula6/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const App = () => {
+
+    return (
+    
+        <>
+
+            <h1>Aula 8</h1>
+            <hr />
+
+            
+
+        </>
+
+    );
+
+};
+
+export default App;
