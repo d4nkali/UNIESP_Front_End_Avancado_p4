dc8b7fdb17423d196e3fb4937cffc504a36026cc
d4nkali <133923419+d4nkali@users.noreply.github.com>
2025-05-16 16:11:40 -0300

    Adicionando Navbar

--- A10-14_05_25/site-uniesp/src/App.jsx
+++ A10-14_05_25/site-uniesp/src/App.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Container } from "react-bootstrap";
 import Inicial from "./pages/Inicial";
+import Navbar from "./components/CustomNavbar";
 
 const App = () => {
     return (
         <div>
-            <Inicial />
+            <BrowserRouter>
+                <Navbar />
+                <Container className="my-4">
+                    <Routes>
+                        <Route path="/" element={<Inicial />}></Route>
+                    </Routes>
+                </Container>
+            </BrowserRouter>
         </div>
     );
 };
--- A10-14_05_25/site-uniesp/src/pages/Inicial.jsx
+++ A10-14_05_25/site-uniesp/src/pages/Inicial.jsx
@@ -1,52 +1,91 @@
 import React from "react";
-import { Card, Col, Row, Container } from "react-bootstrap";
+import { Container, Card, Row, Col } from "react-bootstrap";
 
 const Inicial = () => {
     return (
-        <div>
-            <Container className="py-4">
-                {/* Aqui vai ser o carrossel do banner */}
-                <Card className="mb-4 shadow-sm">
-                    <Card.Body>
-                        <p>
-                            A Faculdade UNIESP é uma instituição comprometida em
-                            fornecer educação de qualidade, com foco no
-                            desenvolvimento profissional e pessoal...
-                        </p>
-                        <p>
-                            Contamos com uma equipe de professores qualificados,
-                            uma infraestrutura moderna e uma metodologia de
-                            ensino que valoriza o aprendizado prático...
-                        </p>
-                        <p>
-                            A seguir, conheça mais sobre as áreas e as
-                            oportunidades que a Faculdade UNIESP oferece para
-                            você.
-                        </p>
-                    </Card.Body>
-                </Card>
+        <Container classname="py-4">
+            {/* Aqui será o carousel do banner */}
 
-                <Row className="g-4">
-                    <Col md={4}>
-                        {/* Card1 */}
-                        <Card className="text-center h-100 shadow-sm">
-                            <Card.Img
-                                variant="top"
-                                src=""
-                                alt="Campus Life"
-                                style={{ height: "200px", objectFit: "cover" }}
-                            />
+            <Card className="mb-4 shadow-sm">
+                <Card.Body>
+                    <p>
+                        A Faculdade UNIESP é uma instituição comprometida em
+                        fornecer educação de qualidade, com foco no
+                        desenvolvimento profissional e pessoal...
+                    </p>
+                    <p>
+                        Contamos com uma equipe de professores qualificados, uma
+                        infraestrutura moderna e uma metodologia de ensino que
+                        valoriza o aprendizado prático...
+                    </p>
+                    <p>
+                        A seguir, conheça mais sobre as áreas e as oportunidades
+                        que a Faculdade UNIESP oferece para você.
+                    </p>
+                </Card.Body>
+            </Card>
 
-                            <Card.Body>
-                                <Card.Title>Vida no Campus</Card.Title>
+            <Row className="g-4">
+                <Col md={4}>
+                    {/* Card 1 */}
+                    <Card className="text-center h-100 shadow=sm">
+                        <Card.Img
+                            variant="top"
+                            src="https://images.unsplash.com/photo-1556761175-4b46a572b786"
+                            alt="Campus Life"
+                            style={{ height: "200px", objectFit: "cover" }}
+                        />
+                        <Card.Body>
+                            <Card.Title>Vida no Campus</Card.Title>
+                            <Card.Text>
+                                Um ambiente vibrande e acolhedor, com
+                                atividades...
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
 
-                                <Card.Text>A...</Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Row>
-            </Container>
-        </div>
+                <Col md={4}>
+                    {/* Card 2 */}
+                    <Card className="text-center h-100 shadow=sm">
+                        <Card.Img
+                            variant="top"
+                            src="https://images.unsplash.com/photo-1571260899304-425eee4c7efc"
+                            alt="Biblioteca"
+                            style={{ height: "200px", objectFit: "cover" }}
+                        />
+                        <Card.Body>
+                            <Card.Title>Biblioteca</Card.Title>
+                            <Card.Text>
+                                Uma biblioteca com um acervo completo e
+                                atualizado, incluindo livros, revistas e
+                                recursos digitais para pesquisa e estudo.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+
+                <Col md={4}>
+                    {/* Card e */}
+                    <Card className="text-center h-100 shadow=sm">
+                        <Card.Img
+                            variant="top"
+                            src="https://images.unsplash.com/photo-1652974731232-efc86a9bd985"
+                            alt="Prédio Principal"
+                            style={{ height: "200px", objectFit: "cover" }}
+                        />
+                        <Card.Body>
+                            <Card.Title>Prédio Principal</Card.Title>
+                            <Card.Text>
+                                O centro de nossa instituição, com salas de aula
+                                modernas e laboratórios equipados para atender
+                                às necessidades dos cursos.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
